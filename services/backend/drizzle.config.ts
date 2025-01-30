58a418d3a422e54b30643a4bf81927a6b0452bd6
Gregory Tsyapa <grinyatsy@gmail.com>
2025-01-30 15:30:55 +0200

    manual migrations env fix

--- services/backend/drizzle.config.ts
+++ services/backend/drizzle.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+import path from "path";
+
+// Construct the absolute path to your .env file
+const envPath = path.resolve(__dirname, "../../.env");
+
+dotenv.config({ path: envPath });
 
 export default defineConfig({
   dialect: "postgresql", // or your respective database dialect
   schema: "./src/db/tables.ts", // Path to your table definitions
   dbCredentials: {
-    host: process.env.DATABASE_HOST ?? "localhost",
+    host: "localhost",
     port: Number(process.env.DATABASE_PORT ?? 5432),
     user: process.env.DATABASE_USER ?? "admin",
     password: process.env.DATABASE_PASSWORD ?? "postgres_pwd",
